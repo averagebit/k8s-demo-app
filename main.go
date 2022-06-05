dae7cb0ee336f664754b24f0510e0acfb1004484
averagebit <averagebit@pm.me>
2022-06-05 06:08:49 +0000

    feat: remove fiber, add dotenv

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	app := fiber.New()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		githubUser := os.Getenv("GITHUB_USER")
+		githubToken := os.Getenv("GITHUB_TOKEN")
+		url := fmt.Sprintf("https://api.github.com/users/%s/repos", githubUser)
+
+		client := &http.Client{}
+
+		res, err := client.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		req.Header.Add("Authorization", "token "+githubToken)
+
+		res, err = client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.WriteHeader(res.StatusCode)
+		w.Write([]byte(res.Status))
 	})
 
-	log.Fatal(app.Listen(":8080"))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
